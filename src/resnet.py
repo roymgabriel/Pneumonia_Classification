d0e59038829ed57a781c0e60311ed738cf8d5ffa
josephslee30 <josephslee30@gmail.com>
2024-04-22 23:17:48 -0400

    Remove Bias

--- src/resnet.py
+++ src/resnet.py
@@ -30,7 +30,7 @@ def build_resnet_model(pretrained=True, fine_tune=True, bayes_type=True, num_cla
                         out_channels=model.conv1.out_channels,
                         kernel_size=model.conv1.kernel_size,
                         stride=model.conv1.stride,
-                        bias=model.conv1.bias
+                        # bias=model.conv1.bias
                     )
         for layer in [model.layer1, model.layer2, model.layer3, model.layer4]:
             for bottleneck in layer:
@@ -39,21 +39,21 @@ def build_resnet_model(pretrained=True, fine_tune=True, bayes_type=True, num_cla
                     out_channels=bottleneck.conv1.out_channels,
                     kernel_size=bottleneck.conv1.kernel_size,
                     stride=bottleneck.conv1.stride,
-                    bias=bottleneck.conv1.bias
+                    # bias=bottleneck.conv1.bias
                 )
                 bottleneck.conv2 = BayesianConv2d(
                     in_channels=bottleneck.conv2.in_channels,
                     out_channels=bottleneck.conv2.out_channels,
                     kernel_size=bottleneck.conv2.kernel_size,
                     stride=bottleneck.conv2.stride,
-                    bias=bottleneck.conv2.bias
+                    # bias=bottleneck.conv2.bias
                 )
                 bottleneck.conv3 = BayesianConv2d(
                     in_channels=bottleneck.conv3.in_channels,
                     out_channels=bottleneck.conv3.out_channels,
                     kernel_size=bottleneck.conv3.kernel_size,
                     stride=bottleneck.conv3.stride,
-                    bias=bottleneck.conv3.bias
+                    # bias=bottleneck.conv3.bias
                 )
 
         # Classifier
